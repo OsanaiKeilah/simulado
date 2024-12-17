e36a16c54ba91010f40cd3cf6091c9e361424717
Keilah Hina <109986056+OsanaiKeilah@users.noreply.github.com>
2024-12-17 20:11:33 -0300

    Add files via upload

---
+++ storage/framework/views/822b9bbad08852216f8187db8fc9dc7e.php
@@ -0,0 +1,55 @@
+
+
+<?php $__env->startSection('title', 'Simulado - Matemática'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <!-- Início do cabeçalho do AdminLTE3 -->
+    <header class="main-header">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+    </header>
+
+    <!-- Conteúdo principal -->
+    <div class="content-wrapper">
+        <section class="content" style="padding: 30px;">
+            <!-- Texto de boas-vindas -->
+            <div style="text-align: center; color: white; margin-top: 80px;">
+                <h1>Bem-vindo, aluno!</h1>
+            </div>
+
+            <div style="text-align: center; color: white; margin-top: 20px;">
+                <p>Clique no botão abaixo para iniciar o teste</p>
+            </div>
+
+            <!-- Botão centralizado -->
+            <div style="text-align: center; margin-top: 20px;">
+                <a href="<?php echo e(route('simulado')); ?>">
+                    <button id="start" onClick="cliqueBtn()">INICIAR</button>
+                </a>
+            </div>
+        </section>
+    </div>
+
+    <!-- Adicionando o estilo CSS para o botão -->
+    <style>
+        button {
+            font-size: 18px;
+            font-weight: 600;
+            background-color: white;
+            border-radius: 12px;
+            border: 0;
+            padding: 10px 20px;
+            cursor: pointer;
+            color: #227930; /* Cor do texto do botão */
+        }
+
+        /* Efeito de hover (opcional) */
+        button:hover {
+            background-color: #f1f1f1; /* Cor de fundo do botão ao passar o mouse */
+        }
+    </style>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\PC-01\simulador\resources\views/home.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/fa00dbe9b28e66b9d58022b022bd823e.php
@@ -0,0 +1,28 @@
+
+
+<?php $__env->startSection('title', 'Resposta do Simulado'); ?>
+
+<?php $__env->startSection('content'); ?>
+
+    <div style="background-color: #227930; padding: 60px; max-width: 1000px; height: 70vh; margin-top: 10px; margin: 0 auto;">
+        <div style="background-color: white; padding: 60px; border-radius: 8px; width: 100%; height: auto; margin: 0 auto;">
+        
+            <header style="text-align: center; color: #227930; margin-top: 120px;">
+                <h1>Resultado da sua Resposta</h1>
+            </header>
+
+            <header style="text-align: center; color: #227930; margin-top: 20px;">
+                <p><?php echo e($mensagem); ?></p>
+            </header>
+
+            <div style="text-align: center; margin-top: 20px;">
+                <a href="<?php echo e(route('simulado')); ?>" style="padding: 10px 20px; background-color: #fff; color: #227930; border-radius: 12px; border: none; cursor: pointer; margin-top: 20px;">
+                    Voltar ao Simulado
+                </a>
+            </div>
+        </div>
+    </div>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\PC-01\Desktop\Simulador-PHP\simulador\resources\views/resposta.blade.php ENDPATH**/ ?>
